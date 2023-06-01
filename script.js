483d731e0c5a7f19946a59f5c3e86b17991a9532
mbasse <matias.basse@hotmail.com>
2023-06-01 18:01:58 -0300

    se termina codigo de simulador

--- script.js
+++ script.js
@@ -4,7 +4,9 @@
     alert('Bienvenido/a a nuestra agencia de viajes')
 
     // utilizo IF para saber en q temporada quiere viajar el pasajero
-    let mes = prompt('Por favor indicanos en que número de mes querés viajar')
+    let mes = prompt('Por favor indicanos en que número de mes que querés viajar')
+        
+    
         if (mes <= 2) {
             alert ('Buena elección. Estás elijiendo la temporada de verano')
         } else if ( (mes >= 3) && (mes < 6)) {
@@ -16,13 +18,26 @@
         } else if ((mes >= 12)){
             alert ('Buena elección. Estás elijiendo la temporada de verano')
         }
-         
-    let pasajeros = prompt ('Ingrese cantidad de pasajeros')
+    
 
-    for (let i = 1; i<=pasajeros ; i++) {
-        nombre = prompt('Ingrese el nombre de cada pasajero')
+    let cantPasajeros = prompt ('Ingrese cantidad de pasajeros')
+     
+    // ciclo para el ingreso de cada pasajero. Esta info no se guarda en ningun lugar por el momento
+    for (let i = 1; i<=cantPasajeros ; i++) {
+        let nombre = prompt('Ingrese el nombre de cada pasajero')
+        
     }
-    return nombre
+    
+    alert ('Perfecto. La cantidad de pasajeros a viajar es de ' + cantPasajeros)
+
+    // info requerida para devolver en el return
+    destino = (prompt('Por favor indicanos a que destino del país te gustaria viajar'))
+
+    mail = prompt ('Ahora te pedimos que nos dejes tu mail.')
+
+    return ('Perfecto. En breve recibiras asesoramiento para viajar el mes ' + mes + ' para ' + cantPasajeros + ' pasajeros. Te enviaremos toda esta info a ' + mail)
+   
+
  }
 
-viajar ()
+alert(viajar ())
